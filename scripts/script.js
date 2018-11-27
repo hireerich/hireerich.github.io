a13b560cb8b1db1150c5d9ce1f3bc832295cbb59
w0244971 <w0244971@nscc.ca>
2018-11-27 00:34:11 -0400

    Most of the portfolio is completed - Erich Gennette

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+/*$(document).scroll(function() {
+		$('.bottomMenu').each(function(i) {
+			var bottom_of_object = $(this).position().top + $(this).outerHeight();
+			var bottom_of_window = $(window).scrollTop() + $(window).height();
+			if(bottom_of_window > bottom_of_object) {
+				$(this).animate({'opacity' : '1'}, 500);
+			}
+		});
+	});
+});*/
